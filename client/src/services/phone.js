2fff90c38c1895f07199b0e508cd1e36e275b984
Philip-jamk <ag4373@student.jamk.fi>
2025-01-04 20:16:54 +0200

    second

--- client/src/services/phone.js
+++ client/src/services/phone.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const baseUrl = "https://part3-phonebook-backend-2.onrender.com/api/persons";
+const baseUrl = "https://part3-ukyk.onrender.com/api/persons";
+// https://part3-ukyk.onrender.com
 
 const getAll = () => {
   const request = axios.get(baseUrl);
